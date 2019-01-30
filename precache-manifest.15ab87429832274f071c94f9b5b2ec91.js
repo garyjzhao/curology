a5b885b63126f290b5b93290c5a0135e5bc06c31
Gary Zhao <gary.j.zhao@gmail.com>
2019-01-30 15:14:26 -0800

    Updates

---
+++ precache-manifest.15ab87429832274f071c94f9b5b2ec91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c7e345e241a0c84a60a",
+    "url": "/curology/static/css/main.3bb8a969.chunk.css"
+  },
+  {
+    "revision": "6c7e345e241a0c84a60a",
+    "url": "/curology/static/js/main.6c7e345e.chunk.js"
+  },
+  {
+    "revision": "fbfa9ac1c04d6f818c50",
+    "url": "/curology/static/js/1.fbfa9ac1.chunk.js"
+  },
+  {
+    "revision": "be80154e123b919a9a02",
+    "url": "/curology/static/js/runtime~main.be80154e.js"
+  },
+  {
+    "revision": "9f04ca9e87eef167b8dcfb484e961988",
+    "url": "/curology/index.html"
+  }
+];
