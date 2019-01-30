da959db85e63cd4d99df075d8f78ca45f4628640
Gary Zhao <gary.j.zhao@gmail.com>
2019-01-30 11:49:39 -0800

    onChange functionality

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
 class Input extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        const searchInput = event.target.value;
+        const { inputChange } = this.props;
+        inputChange(searchInput);
+    }
+
     render() {
         const { placeholder } = this.props;
-        return <input type="text" placeholder={placeholder} />;
+        return (
+            <input
+                type="text"
+                placeholder={placeholder}
+                onChange={this.handleChange}
+            />
+        );
     }
 }
 
