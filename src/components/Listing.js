858b1b6d159e731efb8fb4626c93416268870e41
Gary Zhao <gary.j.zhao@gmail.com>
2019-01-29 20:11:28 -0800

    conditional formatting for the images

--- src/components/Listing.js
+++ src/components/Listing.js
@@ -2,18 +2,37 @@ import React, { Component } from 'react';
 
 export default class Listing extends Component {
     render() {
-        const { title, overview, releaseDate, posterPath } = this.props;
-        return (
-            <li>
+        const {
+            title,
+            overview,
+            releaseDate,
+            posterPath,
+            voteCount,
+        } = this.props;
+        let listingImg;
+
+        if (posterPath == null) {
+            listingImg = <div className="listing__img no-image">No Image</div>;
+        } else {
+            listingImg = (
                 <img
                     className="listing__img"
                     src={`https://image.tmdb.org/t/p/w154${posterPath}`}
                     alt=""
                 />
+            );
+        }
+
+        return (
+            <li>
+                {listingImg}
                 <div className="listing__copy">
                     <h2>{title}</h2>
-                    <p>{releaseDate}</p>
+                    <p className="listing__date">{releaseDate}</p>
                     <p>{overview}</p>
+                    <div className="listing__stars">
+                        <span className="" /> on {voteCount} reviews.
+                    </div>
                 </div>
             </li>
         );
