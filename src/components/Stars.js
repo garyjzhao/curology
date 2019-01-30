13e9a796b49460ebb7e264dbddbbe33c6123385c
Gary Zhao <gary.j.zhao@gmail.com>
2019-01-30 12:36:27 -0800

    streamlined Stars component

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -1,23 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class Stars extends Component {
-    render() {
-        const { voteCount, voteAverage } = this.props;
-        return (
-            <div className="listing__stars">
-                <span>
-                    {voteAverage}
-                    <svg viewBox="0 0 98 92">
-                        <path
-                            stroke="#000"
-                            strokeWidth="5"
-                            d="M49 73.5L22.55 87.406l5.05-29.453-21.398-20.86 29.573-4.296L49 6l13.225 26.797 29.573 4.297-21.4 20.86 5.052 29.452z"
-                            fillRule="evenodd"
-                        />
-                    </svg>
-                </span>
-                /10 on {voteCount} reviews.
-            </div>
-        );
-    }
-}
+const Stars = ({ voteAverage, voteCount }) => (
+    <div className="listing__stars">
+        <span>
+            {voteAverage}
+            <svg viewBox="0 0 98 92">
+                <path
+                    stroke="#000"
+                    strokeWidth="5"
+                    d="M49 73.5L22.55 87.406l5.05-29.453-21.398-20.86 29.573-4.296L49 6l13.225 26.797 29.573 4.297-21.4 20.86 5.052 29.452z"
+                    fillRule="evenodd"
+                />
+            </svg>
+        </span>
+        /10 on {voteCount} reviews.
+    </div>
+);
+
+export default Stars;
